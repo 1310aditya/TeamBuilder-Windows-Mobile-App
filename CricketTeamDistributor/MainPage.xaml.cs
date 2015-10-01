ee5e30d44e3e4950476f1fd8fb2e908c33de352e
Aditya Agrawal <agrawal.aditya@outlook.com>
2015-10-01 20:16:55 +0530

    Resolved runtime image error

--- CricketTeamDistributor/MainPage.xaml.cs
+++ CricketTeamDistributor/MainPage.xaml.cs
@@ -37,13 +37,17 @@ namespace CricketTeamDistributor
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
+            datamodel temp = new datamodel();
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            temp.src = "Assets/cricket-hd-wallpapers.jpg";
+            temp.title = "Cricket";
+            CricketButton.DataContext = temp;
+            
+            temp = new datamodel();
+            temp.src = "Assets/real-madrid-granada.jpg";
+            temp.title = "Football";
+            Football.DataContext = temp;
+                        
         }
 
         
---
+++ CricketTeamDistributor/datamodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketTeamDistributor
+{
+    class datamodel
+    {
+        public string src { get; set; }
+        public string title { get; set; }
+    }
+}
